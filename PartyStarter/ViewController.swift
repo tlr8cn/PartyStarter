d53de50fc92914dd322cef106ce76b5aa12678ab
tlr8cn <tlr8cn@virginia.edu>
2016-04-01 16:40:21 -0700

    first commit

---
+++ PartyStarter/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  PartyStarter
+//
+//  Created by Tyler Robinson on 4/1/16.
+//  Copyright © 2016 Tyler Robinson. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if(FBSDKAccessToken.currentAccessToken() == nil) {
+            print("not logged in..")
+        } else {
+            print("logged in..")
+        }
+        
+        var loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    //Mark: Facebook login
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        if(error == nil) {
+            print("login complete..")
+            self.performSegueWithIdentifier("showNew", sender: self)
+        } else {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("user logged out..")
+    }
+
+}
+
