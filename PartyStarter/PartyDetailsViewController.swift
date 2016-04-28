3cd03b1ad5142c4198b731ce0edd706c88e3de73
tlr8cn <tlr8cn@virginia.edu>
2016-04-28 14:01:32 -0700

    new views added

---
+++ PartyStarter/PartyDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PartyDetailsViewController.swift
+//  PartyStarter
+//
+//  Created by Tyler Robinson on 4/28/16.
+//  Copyright © 2016 Tyler Robinson. All rights reserved.
+//
+
+import UIKit
+
+class PartyDetailsViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Party Title Goes Here"
+        
+    }
+    
+}
---
+++ PartyStarter/PartyListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PartyListViewController.swift
+//  PartyStarter
+//
+//  Created by Tyler Robinson on 4/28/16.
+//  Copyright © 2016 Tyler Robinson. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PartyListViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Your Friends' Parties"
+
+    }
+
+}
